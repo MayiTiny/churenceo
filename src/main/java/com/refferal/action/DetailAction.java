3c6c899b36c48064408ca729dcbcc0cbd14a51a4
YufeiSun <261182674@qq.com>
2015-01-03 23:09:33 +0800

    auto reply

--- src/main/java/com/refferal/action/DetailAction.java
+++ src/main/java/com/refferal/action/DetailAction.java
@@ -23,65 +23,69 @@ import com.refferal.common.Const;
 import com.refferal.entity.JobDescriptionDTO;
 import com.refferal.mail.MailSendInfo;
 import com.refferal.mail.MailService;
+import com.refferal.mail.MailServiceImpl;
 import com.refferal.service.JobDescriptionService;
 
 @Controller
 public class DetailAction {
 
 	private static final Log LOGGER = LogFactory.getLog(DetailAction.class);
-	
+
 	@Autowired
 	private JobDescriptionService jobDescriptionService;
-	
+
 	@Autowired
 	private MailService mailService;
-	
+
 	@RequestMapping("/detail/{id:\\d+}")
-    public ModelAndView detail(@PathVariable(value="id") Integer id) {
+	public ModelAndView detail(@PathVariable(value = "id") Integer id) {
 		ModelAndView mv = new ModelAndView("job_detail");
 		JobDescriptionDTO jd = jobDescriptionService.selectById(id);
 		mv.addObject("jd", jd);
-        return mv;
-    }
+		return mv;
+	}
 
 	@RequestMapping("/upload")
 	@ResponseBody
-    public JSONObject upload(HttpServletRequest request, HttpServletResponse response) {
-		
+	public JSONObject upload(HttpServletRequest request,
+			HttpServletResponse response) {
+
 		JSONObject json = new JSONObject();
-		
-		MultipartHttpServletRequest fileRequest = (MultipartHttpServletRequest)request;
+
+		MultipartHttpServletRequest fileRequest = (MultipartHttpServletRequest) request;
 		List<MultipartFile> files = fileRequest.getFiles("qqfile");
 		String localPath = null;
 		for (MultipartFile file : files) {
-			localPath = Const.FILE_PATH + System.currentTimeMillis() + "/" + file.getOriginalFilename();
+			localPath = Const.FILE_PATH + System.currentTimeMillis() + "/"
+					+ file.getOriginalFilename();
 			File localFile = new File(localPath);
 			localFile.mkdirs();
-            try {
-            	file.transferTo(localFile);
+			try {
+				file.transferTo(localFile);
 			} catch (IOException e) {
 				LOGGER.error("上传简历文件失败！" + localFile, e);
 				json.put("success", false);
 				return json;
 			}
-        }
-		
+		}
+
 		json.put("success", true);
 		json.put("path", localPath);
 		return json;
-    }
-	
+	}
+
 	@RequestMapping("/deliver")
 	@ResponseBody
-    public JSONObject deliver(HttpServletRequest request, HttpServletResponse response) {
-		
+	public JSONObject deliver(HttpServletRequest request,
+			HttpServletResponse response) {
+
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String path = request.getParameter("resumePath");
 		String title = request.getParameter("title");
-		
+
 		JSONObject json = new JSONObject();
-		
+
 		MailSendInfo mailInfo = new MailSendInfo();
 		mailInfo.setMailServerHost("smtp.qq.com");
 		mailInfo.setMailServerPort("25");
@@ -94,11 +98,33 @@ public class DetailAction {
 		mailInfo.setContent("您好，这是我的简历，请查收。我的邮箱是：" + email);
 		mailInfo.setAttachFileName(path.substring(path.lastIndexOf("/") + 1));
 		mailInfo.setAttachFilePath(path);
-		
+
 		mailService.sendTextMail(mailInfo);
-		
+		try {
+			MailSendInfo info = new MailSendInfo();
+			String tmp[] = title.split("-");
+			info.setMailServerHost("smtp.qq.com");
+			info.setMailServerPort("25");
+			info.setValidate(true);
+			info.setUserName("churenceo@foxmail.com");
+			info.setPassword("dragon7910");// 您的邮箱密码
+			info.setFromAddress("churenceo@foxmail.com");
+			info.setToAddress(email);
+			info.setSubject("【搬砖网】" + tmp[2] + "-" + tmp[3] + "投递通知");
+			info.setContent(name
+					+ "，你好!</br>你选择的"
+					+ tmp[2]
+					+ "-"
+					+ tmp[3]
+					+ "职位投递成功，搬砖君评估之后会转给对应公司的内推人，</br>更多信息请关注我们的微信：churenceo。</br><img src=\"http://www.neitui.me/module/neitui/images/qrcode.png\">");
+			// 这个类主要来发送邮件
+			MailServiceImpl sms = new MailServiceImpl();
+			sms.sendHtmlMail(info);// 发送文体格式
+		} catch (Exception e) {
+			LOGGER.error("发送回复右键失败！");
+		}
 		return json;
-		
+
 	}
-	
+
 }
